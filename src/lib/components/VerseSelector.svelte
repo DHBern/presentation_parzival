aae7d5c6f99e009e2b3bf3d89d40529393bf9cab
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-11-02 22:28:33 +0100

    a11y fix

--- src/lib/components/VerseSelector.svelte
+++ src/lib/components/VerseSelector.svelte
@@ -76,7 +76,10 @@
 			bind:this={verse}
 			value={Number(coordinates[1])}
 		/>-<input type="text" placeholder="Zusatz" class="input max-w-20" bind:value={additional} />
-		<button class="btn-icon variant-filled btn-icon-sm flex-shrink-0 flex-grow-0">
+		<button
+			aria-label="suchen"
+			class="btn-icon variant-filled btn-icon-sm flex-shrink-0 flex-grow-0"
+		>
 			<i class="fa-solid fa-magnifying-glass"></i></button
 		>
 	{/if}
