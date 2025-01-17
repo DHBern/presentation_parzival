e5d6100aab5391a43a3b8aac5ca3ca851417577c
flicksolutions <sebastian@flick.solutions>
2025-01-17 17:28:28 +0100

    decide for functionality

--- src/routes/suche/+page.svelte
+++ src/routes/suche/+page.svelte
@@ -1,26 +1,22 @@
 <script>
-	import { api } from '$lib/constants';
+	import { onMount } from 'svelte';
 	import { minisearch } from '$lib/minisearch.svelte';
-	/** @type {{data: import('./$types').PageData}} */
-	let { data } = $props();
-
-	let allDocsAdded;
-
-	$effect(() => {
+	import { api } from '$lib/constants';
+	let docsAdded = $state(Promise.resolve(!minisearch.documentCount));
+	onMount(async () => {
 		if (!minisearch.documentCount) {
-			console.log('fetching search index');
-			fetch(`${api}/json/search-index.json`)
-				.then((r) => r.json())
-				.then((index) => {
-					allDocsAdded = /** @type {Promise<void>} */ (
-						new Promise((resolve) => {
-							minisearch.addAllAsync(index.docs, { chunkSize: 50000 }).then(() => {
-								console.log(minisearch.search('parzival'));
-								resolve();
-							});
-						})
-					);
-				});
+			console.log('fetching and adding docs');
+			const { docs } = await fetch(`${api}/json/search-index.json`).then((r) => r.json());
+			docsAdded = /** @type {Promise<boolean>} */ (
+				new Promise((resolve) => {
+					minisearch.addAllAsync(docs, { chunkSize: 50000 }).then(() => {
+						console.log('all documents added');
+						resolve(true);
+					});
+				})
+			);
 		}
 	});
 </script>
+
+<button onclick={() => console.log(minisearch.search('feirefiz'))}>Log data</button>
