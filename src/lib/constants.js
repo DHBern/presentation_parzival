386b4aac1398be4835d5d41139453d3d9e3afa4c
flicksolutions <sebastian@flick.solutions>
2025-01-20 20:59:21 +0100

    Fassungen as Label

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,4 +4,4 @@ export const teipb = `http://localhost:8081/exist/apps/parzival/api`;
 
 export const iiif = `https://iiif.ub.unibe.ch/image/v3/Parzival`;
 
-export const summaryLabel = 'Fassung';
+export const summaryLabel = 'Fassungen';
