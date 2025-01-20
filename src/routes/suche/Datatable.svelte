b5d365be5c53a2cdc3a5f3b9646e93714aa4400e
flicksolutions <sebastian@flick.solutions>
2025-01-20 17:05:55 +0100

    styling

--- src/routes/suche/Datatable.svelte
+++ src/routes/suche/Datatable.svelte
@@ -3,37 +3,62 @@
 	import { base } from '$app/paths';
 	let { searchResults } = $props();
 	const table = new TableHandler(searchResults, { rowsPerPage: 100 });
+	let filter = [
+		table.createFilter('humanReadableSigil'),
+		table.createFilter('d'),
+		table.createFilter('verse'),
+		table.createFilter('content')
+	];
 </script>
 
-<Datatable {table}>
-	<table>
+<div class="table-container">
+	<table class="table table-hover">
 		<thead>
 			<tr>
 				<ThSort {table} field="humanReadableSigil">Sigle</ThSort>
-				<ThSort {table} field="d">Dreißiger</ThSort>
-				<ThSort {table} field="verse">Vers</ThSort>
+				<ThSort {table} field={(r) => Number(r.d)}>Dreißiger</ThSort>
+				<ThSort {table} field={(r) => Number(r.verse)}>Vers</ThSort>
 				<ThSort {table} field="content">Inhalt</ThSort>
 			</tr>
 			<tr>
-				<ThFilter {table} field="humanReadableSigil" />
-				<ThFilter {table} field="d" />
-				<ThFilter {table} field="verse" />
-				<ThFilter {table} field="content" />
+				{#each filter as f}
+					<th class="py-1">
+						<input
+							placeholder="Filter..."
+							class="input p-0.5"
+							type="text"
+							bind:value={f.value}
+							oninput={() => f.set()}
+						/>
+					</th>
+				{/each}
 			</tr>
 		</thead>
 		<tbody>
 			{#each table?.rows as row}
 				<tr>
-					<td>{row?.humanReadableSigil}</td>
-					<td>{row?.d}</td>
-					<td>{row?.verse}</td>
+					<td class="table-cell-fit">{row?.humanReadableSigil}</td>
+					<td class="table-cell-fit">{row?.d}</td>
+					<td class="table-cell-fit">{row?.verse}</td>
 					<td
-						><a href={`${base}/textzeugen/${row.humanReadableSigil}/${row.d}/${row.verse}`}
-							>{row?.content}</a
+						><a href={`${base}/textzeugen/${row.sigla}/${row.d}/${row.verse}`}>{row?.content}</a
 						></td
 					>
 				</tr>
 			{/each}
 		</tbody>
 	</table>
-</Datatable>
+</div>
+
+<div class="flex space-x-1 my-4">
+	{#each table.pagesWithEllipsis as page}
+		<button
+			type="button"
+			class:variant-filled-primary={page === table.currentPage}
+			class="btn variant-filled"
+			onclick={() => table.setPage(page)}
+		>
+			{page ?? '...'}
+		</button>
+	{/each}
+</div>
