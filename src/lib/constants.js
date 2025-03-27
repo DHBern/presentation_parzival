a643ff124b5a8bb3f2062e00981e9907c59883fb
domsteinbach <dominique.steinbach@unibas.ch>
2025-03-27 19:05:50 +0100

    fix: project slug to lower case

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,7 @@ export const api = `https://dhbern.github.io/parzival-static-api/api`;
 
 export const teipb = `http://localhost:8081/exist/apps/parzival/api`;
 
-export const iiif = `https://iiif.ub.unibe.ch/image/v3/Parzival`;
+export const iiif = `https://iiif.ub.unibe.ch/image/v3/parzival`;
 
 export const summaryLabel = 'Fassungen';
 
