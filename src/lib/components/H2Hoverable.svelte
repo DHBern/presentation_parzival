ee13920fb21fe46b329d3487791e0b5a835dad2d
vvvyyynet <levyn.buerki@unibe.ch>
2025-06-23 12:05:33 +0200

    feat: allow classes propagation in H2Hoverable

--- src/lib/components/H2Hoverable.svelte
+++ src/lib/components/H2Hoverable.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { Link } from '@lucide/svelte';
-	let { hoveredH2 = $bindable(), id, children } = $props();
+	let { hoveredH2 = $bindable(), id, classes="", children } = $props();
 </script>
 
 <h2
 	{id}
+	class={classes}
 	onmouseenter={() => {
 		hoveredH2.value = id;
 	}}
