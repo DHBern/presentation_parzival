3bb530ae08bd5bf3725a8e2fb670acf3a318e85f
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-07 18:06:04 +0200

    fix: switch from lucide to fontawesome

--- src/lib/components/H2Hoverable.svelte
+++ src/lib/components/H2Hoverable.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { Link } from '@lucide/svelte';
+	import '@fortawesome/fontawesome-free/css/solid.min.css';
+	import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 	let { id, classes="", children } = $props();
 	let isHovered = $state(false);
 </script>
@@ -16,6 +17,6 @@
 >
 	{@render children()}
 	{#if isHovered}
-		<a href={`#${id}`} class="inline-block"><Link /></a>
+		<a href={`#${id}`} class="inline-block"><i class="fa-solid fa-link"></i></a>
 	{/if}
 </h2>
