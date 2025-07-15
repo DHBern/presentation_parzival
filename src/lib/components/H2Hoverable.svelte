6a5bff53fe07121c87d08e10e50e74f3c461250c
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-15 08:52:45 +0200

    chore: prettify

--- src/lib/components/H2Hoverable.svelte
+++ src/lib/components/H2Hoverable.svelte
@@ -1,9 +1,12 @@
 <script>
 	import slugify from 'slugify';
 	import '@fortawesome/fontawesome-free/css/all.css';
-	let { id = null, name = '', classes = '', children = null, locale='de' } = $props();
-	id = name ? slugify(name, { locale: locale, lower: true, strict: true }) : id ? id : Math.floor(Math.random() * 100000)
-;
+	let { id = null, name = '', classes = '', children = null, locale = 'de' } = $props();
+	id = name
+		? slugify(name, { locale: locale, lower: true, strict: true })
+		: id
+			? id
+			: Math.floor(Math.random() * 100000);
 </script>
 
 <h2 {id} class={classes}>
