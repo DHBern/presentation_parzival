41c82dbec01c3f0b55389cb145325d1883785cc7
vvvyyynet <levyn.buerki@unibe.ch>
2025-07-18 08:58:07 +0200

    fix: fix link in menu to dreissiger-matrix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,7 @@
 
 	const pages = [
 		{ slug: 'Einführung', path: '/einfuehrung' },
-		{ slug: 'Dreißiger-Matrix', path: `${base}/#edition-nach-dreissigern` },
+		{ slug: 'Dreißiger-Matrix', path: `/#edition-nach-dreissigern` },
 		{ slug: 'English Presentation', path: '/englishpresentation' },
 		{ slug: 'Handschriftenverzeichnis', path: '/hsverz' },
 		{ slug: 'Mitarbeitende', path: '/mitarbeitende' },
