a85d3194a92b1810a0184c0b399f080dfd08c3cb
flicksolutions <sebastian@flick.solutions>
2025-11-06 21:24:32 +0100

    setting prerender to false in the rest routes directly

--- src/routes/einzelverssynopse/[...rest]/+page.js
+++ src/routes/einzelverssynopse/[...rest]/+page.js
@@ -3,3 +3,4 @@ import { error } from '@sveltejs/kit';
 export const load = ({ params }) => {
 	error(404, `Vers ${params.rest} liegt außerhalb des gültigen Bereiches 1.1 bis 827.30`);
 };
+export const prerender = false;
--- src/routes/fassungen/[...rest]/+page.js
+++ src/routes/fassungen/[...rest]/+page.js
@@ -3,3 +3,5 @@ import { error } from '@sveltejs/kit';
 export const load = ({ params }) => {
 	error(404, `Dreißiger ${params.rest} liegt außerhalb des gültigen Bereiches 1 bis 827`);
 };
+
+export const prerender = false;
