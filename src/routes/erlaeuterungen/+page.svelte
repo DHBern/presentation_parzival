b30a3d8f78e1a855901d14b3d8d519e39d9e0dc5
domsteinbach <dominique.steinbach@unibas.ch>
2025-11-26 13:48:19 +0100

    first set of examples

--- src/routes/erlaeuterungen/+page.svelte
+++ src/routes/erlaeuterungen/+page.svelte
@@ -132,9 +132,21 @@
 		Überschriften mit -01 versehen sind.
 	</p>
 
-	<p>
-		Paratexte wie Notizen, Marginalien, Kustoden und Reklamanten werden kursiviert wiedergegeben.
-	</p>
+	<div class="mb-6">
+		<p>
+			Paratexte wie Notizen, Marginalien, Kustoden und Reklamanten werden kursiviert wiedergegeben.
+		</p>
+
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6">
+			<a href="https://dhbern.github.io/presentation_parzival/textzeugen/d/8/1"
+			   target="_blank"
+			   class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+			>D 8.1</a>
+			<div class="mt-2">
+				<span class="content"><span class="tei-name"><span data-ref="regp:p0094"><span class="tei-seg9 initial Farbe: blau">G</span>Ahmvͦreth</span></span> ſprach ave ſan. <span class="tei-note3 marginalia">Gahmuret</span></span>
+			</div>
+		</div>
+	</div>
 
 	<p>Doppelt auftretende Verse erhalten die Ergänzung -r.</p>
 
@@ -158,19 +170,18 @@
 		Strich (|) markiert. Worttrennungen mit Trennungszeichen werden mit =| wiedergegeben.
 	</p>
 
-	<div>
-		<h4 class="h4">Initialen</h4>
+	<div class="mb-6">
 		<p>
 			Initialen: rot, größere Schrift.
 		</p>
 
-		<div class="relative border rounded-xl p-4 bg-surface-50 shadow-sm mt-6">
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6">
 
 			<!-- link label -->
 			<a
 				href="https://dhbern.github.io/presentation_parzival/textzeugen/d/5/29?"
 				target="_blank"
-				class="absolute -top-3 left-4 bg-surface-50 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+				class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
 			>
 				D 5.29
 			</a>
@@ -186,19 +197,16 @@
 		</div>
 	</div>
 
-	<div>
-		<h4 class="h4">Prachtinitialen</h4>
+	<div class="mb-6">
 		<p>
 			Prachtinitialen: rot, größere Schrift, Goldhintergrund.
 		</p>
 
-		<div class="relative border rounded-xl p-4 bg-surface-50 shadow-sm mt-6">
-
-			<!-- link label -->
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6">
 			<a
 				href="https://dhbern.github.io/presentation_parzival/textzeugen/d/1/01-01?"
 				target="_blank"
-				class="absolute -top-3 left-4 bg-surface-50 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+				class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
 			>
 				D 1.1
 			</a>
@@ -214,17 +222,40 @@
 		</div>
 	</div>
 
-	<p>
-		Nicht ausgeführte Initialen: rot, kleinere Schrift. Hat der Schreiber den Initialbuchstaben
-		vorgeschrieben, wird dieser eingesetzt (z. B.: DArnach), in allen anderen Fällen steht der
-		Stellvertreter x (z. B.: <span class="tei-seg1 not-executed">x</span>er).
-	</p>
+	<div class="mb-6">
+		<p>
+			Nicht ausgeführte Initialen: rot, kleinere Schrift. Hat der Schreiber den
+			Initialbuchstaben vorgeschrieben, wird dieser eingesetzt. In allen anderen Fällen
+			steht der Stellvertreter <span class="tei-seg1 not-executed">x</span>.
+		</p>
 
-	<p>
-		Auszeichnungen in den Handschriften (Rubrizierungen u. a.): orangefarbig (z. B.: <span
-			class="tei-hi1 red rot">G</span
-		>amuret).
-	</p>
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6">
+			<a href="https://dhbern.github.io/presentation_parzival/textzeugen/fr31/446/1"
+			   target="_blank"
+			   class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm">
+				Fr. 31 446.1
+			</a>
+
+			<div class="mt-2">
+				<div class="tei-l" id="fr31_446.01">
+					<span class="verse" data-verse="446.01">446.1<sup></sup></span>
+					<span class="content">
+						<span class="tei-seg1 not-executed">x</span>werſ rvͦchet hoeren dem tvͦn ich kvnt</span>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="mb-6">
+		<p>Auszeichnungen in den Handschriften (Rubrizierungen u. a.) werden orangefarbig wiedergegeben.</p>
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
+			<a href="https://dhbern.github.io/presentation_parzival/textzeugen/u/731/13" target="_blank" class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm">U 731.13</a>
+			<div class="mt-2">
+				<span class="content">Der kuͦnec <span class="tei-name"><span data-ref="regp:p0107"><span class="tei-hi1 red rot">G</span>ramoflanz</span></span> in bot</span>
+			</div>
+		</div>
+	</div>
 
 	<p>
 		Versalien und Majuskeln werden in Fettdruck wiedergegeben (z. B.: <span
@@ -234,73 +265,116 @@
 
 	<p>Überschriften: rotfarbig (z. B.: <span class="tei-hi1 red rot">Her parcifal</span>).</p>
 
-	<div>
-		<h4 class="h4">Vom Schreiber ausgeführte Korrekturen</h4>
-		<p>Das korrigierte Wort weist hellgraue Farbe auf (für
+	<div class="mb-6">
+		<p>Vom Schreiber ausgeführte Korrekturen: Das korrigierte Wort weist hellgraue Farbe auf (für
 			unlesbare Buchstaben steht *), das Korrekturresultat wird in grüner Farbe angezeigt.
 		</p>
 
-		<div class="relative border rounded-xl p-4 bg-surface-50 shadow-sm mt-6">
-
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
 			<a
 				href="https://dhbern.github.io/presentation_parzival/textzeugen/d/747/18?"
 				target="_blank"
-				class="absolute -top-3 left-4 bg-surface-50 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+				class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
 			>
 				D 747.18
 			</a>
 			<div class="mt-2">
-		<span class="tei-content">
-			<span class="content">
-				<span class="tei-subst2 subst_sr">
-					<span class="tei-del2 deleted">daz</span>
-					<span class="tei-add2 added">
-						da<span class="tei-hi3 rasure rasur"><span class="tei-gap5 gap">-*-</span></span>
+				<span class="tei-content">
+					<span class="content">
+						<span class="tei-subst2 subst_sr">
+							<span class="tei-del2 deleted">daz</span>
+							<span class="tei-add2 added">
+								da<span class="tei-hi3 rasure rasur"><span class="tei-gap5 gap">-*-</span></span>
+							</span>
+						</span>
+						mvͦz glichiv ſcanze ſten.
 					</span>
 				</span>
-				mvͦz glichiv ſcanze ſten.
+			</div>
+		</div>
+		<div class="mb-6">
+			<p>Vom Schreiber eingefügte Nachträge werden in grüner Farbe dargestellt.</p>
+			<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6">
+				<a
+						href="https://dhbern.github.io/presentation_parzival/textzeugen/u/731/12"
+						target="_blank"
+						class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+				>
+					U 731.12
+				</a>
+				<div class="mt-2">
+					<span class="content"><span class="tei-add1 sr">Der wuͦnſchte die nacht vor den dac</span></span>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="mb-6">
+		<p>
+			Von einem späteren Korrektor ausgeführte Korrekturen: Das korrigierte Wort weist hellgraue
+			Farbe auf (für unlesbare Buchstaben steht *), das Korrekturresultat wird in türkiser Farbe
+			angezeigt.
+		</p>
+
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
+			<a href="https://dhbern.github.io/presentation_parzival/textzeugen/d/53/18"
+			   target="_blank"
+			   class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+			>D 53.18</a>
+			<div class="mt-2">
+				<span class="content">alſ al die boͮme trvͦgent <span class="tei-subst1 subst_ls"><span class="tei-del2 deleted">got</span><span class="tei-add2 added">golt</span></span>.</span>
+			</div>
+		</div>
+		<p>Von einem späteren Korrektor ausgeführte Nachträge werden in türkiser Farbe dargestellt.</p>
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mb-6">
+			<div class="mt-2">
+			<span class="tei-content">
+				<span class="content">
+					<span class="subst_ls">vohet hie sine kintheit an</span>
+				</span>
 			</span>
-		</span>
+			</div>
+		</div>
+		<p>Wenn die Korrekturen durch Rasur erfolgen, stehen die korrigierten Buchstaben und Wörter auf dunkelgrauem Hintergrund.</p>
+		<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
+			<a href="https://dhbern.github.io/presentation_parzival/textzeugen/v/731/12" target="_blank" class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm">V 731.12</a>
+			<div class="mt-2">
+				<span class="content">Der wúnſchet <span class="tei-subst1 subst_ls"><span class="tei-del2 deleted">*</span><span class="tei-add2 added"><span class="tei-hi3 rasure rasur">der</span></span></span> naht fúr den tag</span>
 			</div>
 		</div>
 	</div>
 
 	<p>
-		Von einem späteren Korrektor ausgeführte Korrekturen: Das korrigierte Wort weist hellgraue Farbe
-		auf (für unlesbare Buchstaben steht *), das Korrekturresultat wird in türkiser Farbe (z. B.:
+		Ersatzlos getilgte Wörter werden in grauer Farbe und durchgestrichen wiedergegeben
+		(z. B.: <span class="del_sr">hie</span>). Die Zeichenfolge
 		<span class="tei-subst1 subst_ls"
-			><span class="tei-del2 deleted">demut</span><span class="tei-add2 added"
-				><span class="tei-hi3 rasure rasur">demuͦt</span></span
-			></span
-		>) angezeigt.
+		><span class="tei-del2 deleted">+</span><span class="tei-add2 added"
+		><span class="tei-hi3 rasure rasur"><span class="tei-gap5 gap">-*-</span></span></span
+		></span
+		>
+		steht für ein ersatzlos getilgtes Kreuz, das die Funktion einer Marginalie einnimmt.
 	</p>
 
-	<p>
-		Von einem späteren Korrektor ausgeführte Nachträge: türkise Farbe (z. B. <span class="subst_ls"
-			>vohet hie sine kintheit an</span
-		>).
-	</p>
-
-	<p>
-		Wenn die Korrekturen durch Rasur erfolgen, stehen die korrigierten Buchstaben und Wörter auf
-		dunkelgrauem Hintergrund (z. B.: Dar <span class="tei-subst1 subst_ls"
-			><span class="tei-del2 deleted">*</span><span class="tei-add2 added"
-				><span class="tei-hi3 rasure rasur"> inne</span></span
-			></span
-		>).
-	</p>
-
-	<p>
-		Ersatzlos getilgte Wörter werden in grauer Farbe und durchgestrichen wiedergegeben (z. B.: <span
-			class="del_sr">hie</span
-		>). Die Zeichenfolge
-		<span class="tei-subst1 subst_ls"
+	<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
+		<a
+			href="https://dhbern.github.io/presentation_parzival/textzeugen/v/141/16"
+			target="_blank"
+			class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+		>
+			V 141.16
+		</a>
+		<div class="mt-2">
+		<span class="content">
+			<span class="del_sr">hie</span>
+			<span class="tei-subst1 subst_ls"
 			><span class="tei-del2 deleted">+</span><span class="tei-add2 added"
-				><span class="tei-hi3 rasure rasur"><span class="tei-gap5 gap">-*-</span></span></span
+			><span class="tei-hi3 rasure rasur"><span class="tei-gap5 gap">-*-</span></span></span
 			></span
-		> (z. B. in V, 141.16) steht für ein ersatzlos getilgtes Kreuz, das die Funktion einer Marginalie
-		einnimmt.
-	</p>
+			>
+		</span>
+		</div>
+	</div>
 
 	<p>
 		Diakritische Zeichen werden als solche dargestellt. Zirkumflex wird grundsätzlich gemäß der
@@ -309,20 +383,44 @@
 	</p>
 
 	<p>
-		Superskripte, die nicht eindeutig identifizierbar sind, werden als übergeschriebene "x" (z. B.:
-		uͯ) wiedergegeben.
+		Superskripte, die nicht eindeutig identifizierbar sind, werden als übergeschriebene „x“
+		(z. B.: uͯ) wiedergegeben.
 	</p>
 
+	<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
+		<a
+			href="https://dhbern.github.io/presentation_parzival/textzeugen/l/8/05"
+			target="_blank"
+			class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+		>
+			L 8.05
+		</a>
+		<div class="mt-2">
+			<span class="content">An guter zuͯht von hoher art</span>
+		</div>
+	</div>
 	<p>
 		Unlesbare Zeichen werden mit : bei einem, mit :: bei zwei und mit ::: bei drei oder mehr
-		unleserlichen Buchstaben angegeben. Textverlust aufgrund materieller Beeinträchtigung (z. B.
-		Fragmentverlust) wird in jedem Fall mit ::: bezeichnet.
+		unleserlichen Buchstaben angegeben. Textverlust aufgrund materieller Beeinträchtigung
+		(z. B. Fragmentverlust) wird in jedem Fall mit ::: bezeichnet.
 	</p>
+
+	<div class="relative border rounded-xl p-4 bg-gray-100 dark:preset-filled-surface-500 shadow-sm mt-6 mb-6">
+		<a
+			href="https://dhbern.github.io/presentation_parzival/textzeugen/d/3/27"
+			target="_blank"
+			class="absolute -top-3 left-4 bg-gray-100 dark:preset-filled-surface-500 px-2 text-primary-600 text-sm font-medium rounded-md border border-primary-200 shadow-sm"
+		>
+			D 3.27
+		</a>
+		<div class="mt-2">
+			<span class="content">da fv̂re e<span class="tei-gap1 gap">:</span>n l<span class="tei-gap2 gap">::</span>g<span class="tei-gap4 gap">:::</span></span>		</div>
+	</div>
 
 	<p>
 		Wenn der materielle Befund des Textzeugen von der Darstellung der digitalen Reproduktion
-		abweicht, wird ersterer in der Transkription berücksichtigt (z. B. in Fragment 5, Bl. 4rc, Vers
-		446.3, wo das Reimwart <i>zal</i> in der Reproduktion auf einem umgeklappten Seitenteil steht und
+		abweicht, wird ersterer in der Transkription berücksichtigt (z. B. in <a href="https://dhbern.github.io/presentation_parzival/textzeugen/fr5/446/03?" target="_blank">Fragment 5, Bl. 4rc, Vers
+		446.3</a>, wo das Reimwart <i>zal</i> in der Reproduktion auf einem umgeklappten Seitenteil steht und
 		nicht sichtbar ist).
 	</p>
 
