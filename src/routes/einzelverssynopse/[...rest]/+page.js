cc7ea0ccef9383428e5f27eadd4d9bb29a48ef51
flicksolutions <sebastian@flick.solutions>
2025-12-07 19:56:50 +0100

    Improve einzelverssynopse

--- src/routes/einzelverssynopse/[...rest]/+page.js
+++ src/routes/einzelverssynopse/[...rest]/+page.js
@@ -1,6 +1,9 @@
 import { error } from '@sveltejs/kit';
 
 export const load = ({ params }) => {
-	error(404, `Vers ${params.rest} liegt außerhalb des gültigen Bereiches 1.1 bis 827.30`);
+	error(
+		404,
+		`Der Vers ${params.rest.replaceAll('/', '.')} liegt außerhalb des gültigen Bereiches 1.1 bis 827.30`
+	);
 };
 export const prerender = false;
