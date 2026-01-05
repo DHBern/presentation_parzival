9fb8d417ea7c59dd427e27a4130f26992430aed2
flicksolutions <sebastian@flick.solutions>
2026-01-05 21:20:26 +0100

    add validation

--- src/lib/components/PageSelector.svelte
+++ src/lib/components/PageSelector.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { goto, preloadData } from '$app/navigation';
+	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 
 	/** @type {{targetPath?: string, coordinates?: [String | boolean, String | boolean]}} */
@@ -14,21 +14,29 @@
 	let rvSuffix = $derived(idParts[3]);
 	let page = $derived(`${Number(idParts[2])}${rvSuffix}`);
 
-	const findthirties = async (/** @type {String | undefined} */ page) => {
+	const findVerse = async (/** @type {String | undefined} */ page) => {
 		if (page) {
-			return (await meta)
-				.find(
-					(pageMeta) =>
-						pageMeta.id ===
-						`${handle}${page.padStart(page[page.length - 1] === 'r' || page[page.length - 1] === 'v' ? 4 : 3, '0')}`
-				)
-				.l.replace('.', '/');
+			const found = (await meta).find(
+				(pageMeta) =>
+					pageMeta.id ===
+					`${handle}${page.padStart(page[page.length - 1] === 'r' || page[page.length - 1] === 'v' ? 4 : 3, '0')}`
+			);
+			if (found?.l) {
+				return found?.l.replace('.', '/');
+			} else {
+				return false;
+			}
 		}
 		return '';
 	};
 
 	let getTargetUrl = async () => {
-		return `${base}${targetPath}/${await findthirties(pageField?.value)}`;
+		const verse = await findVerse(page);
+		if (verse) {
+			return `${base}${targetPath}/${verse}`;
+		} else {
+			return false;
+		}
 	};
 </script>
 
@@ -37,7 +45,15 @@
 	onsubmit={(e) => {
 		e.preventDefault();
 		getTargetUrl().then((url) => {
-			goto(url);
+			if (url) {
+				goto(url);
+			} else {
+				//invalid page input
+				if (pageField) {
+					pageField.setCustomValidity(`${rvSuffix ? 'Blatt' : 'Seite'} nicht gefunden`);
+					pageField.reportValidity();
+				}
+			}
 		});
 	}}
 >
@@ -45,9 +61,12 @@
 	<input
 		type="text"
 		placeholder="Seite oder Blatt"
-		class="input max-w-20"
+		class="input max-w-20 invalid:bg-error-500"
 		bind:this={pageField}
 		bind:value={page}
+		oninput={() => {
+			pageField?.validity.customError && pageField.setCustomValidity('');
+		}}
 	/>
 	<!-- <button aria-label="anzeigen" class="btn preset-filled btn-sm shrink-0 grow-0">Anzeigen</button> -->
 </form>
