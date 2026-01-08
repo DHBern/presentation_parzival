f93eca0eaf1a3c3b1aebced6a5d3defe2067292d
domsteinbach <dominique.steinbach@unibas.ch>
2026-01-08 14:01:50 +0100

    refactor: include aria-label

--- src/lib/components/ExpandableContent.svelte
+++ src/lib/components/ExpandableContent.svelte
@@ -29,6 +29,7 @@
 		class="mt-4 text-sm font-semibold anchor-font-color"
 		aria-controls={contentId}
 		aria-expanded={open}
+		aria-label= "Textausschnitt erweitern/reduzieren"
 		on:click={() => (open = !open)}
 	>
 		{open ? labelLess : labelMore}
