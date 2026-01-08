3dd3f50ac7beecab5bc2ff1c75ee4133f2c0472d
domsteinbach <dominique.steinbach@unibas.ch>
2026-01-08 14:30:53 +0100

    chore: switch to line-clamp-3

--- src/routes/fassungen/[thirties=thirties]/+page.svelte
+++ src/routes/fassungen/[thirties=thirties]/+page.svelte
@@ -422,7 +422,7 @@
 		</button>
 	{/snippet}
 	<h1 class="h1 my-4">Fassungsedition</h1>
-	<ExpandableContent clampClass = 'line-clamp-2' class="mb-4">
+	<ExpandableContent clampClass = 'line-clamp-3' class="mb-4">
 		<ErlaeuterungenFassungsedition />
 	</ExpandableContent>
 	<div class="grid gap-6 md:grid-cols-2 md:my-8">
