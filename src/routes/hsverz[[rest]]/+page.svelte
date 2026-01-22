e62a48e4c8bd7c53b04ff3e008df84dcb8013616
domsteinbach <dominique.steinbach@unibas.ch>
2026-01-22 17:57:49 +0100

    chore: content changes

--- src/routes/hsverz[[rest]]/+page.svelte
+++ src/routes/hsverz[[rest]]/+page.svelte
@@ -7,7 +7,20 @@
 </script>
 
 <div class="container mx-auto typography">
-	<h1 class="h1">Parzival-Handschriften und Fragmente zu den Editionsproben</h1>
+	<h1 class="h1">Verzeichnis der Textzeugen des ›Parzival‹</h1>
+
+	<p>
+		Die Siglen der im Folgenden aufgelisteten Textzeugen von Wolframs ›Parzival‹ – 16 (mehr oder
+		weniger vollständige) Handschriften, 1 Wiegendruck und 72 Handschriftenfragmente – entsprechen
+		den Angaben bei KLEIN 2011, S. 943–959, die Siglen in runden Klammern folgen der Systematik bei
+		HARTL 1952, S. XLIV–LXIII. Die fortlaufende Zählung der Fragmente schließt sich BONATH/LOMNITZER
+		1989 an, mit den älteren Siglen in runden Klammern nach ULZEN 1974, S. VIf. Die
+		Fassungszugehörigkeit einiger Textzeugen, die in der Edition eindeutig einer Fassung zugeordnet
+		worden sind, ist unsicher und entsprechend markiert, z.B. Fr. 2 („wohl *D“). Die einzelnen
+		Abschnitte enthalten ggf. Verweise vor allem auf den Handschriftencensus und weitere
+		online-Datenbanken sowie auf bestehende Digitalfaksimiles.
+	</p>
+
 	{#each [...sigla.codices, ...sigla.fragments] as { "handle": handle, "sigil": sigil, "info-h1": info_h2, "info-h2": loc, info }}
 		<!-- set the id of h2 (for fraction use handles, for codices use case-sensitive sigils) -->
 		{@const id = handle[0] === 'f' ? handle : sigil}
@@ -19,66 +32,71 @@
 			<p>{@html info}</p>
 		{/if}
 	{/each}
+
 	<H2Hoverable id="Referenzen">Zu dieser Liste der Handschriften vergleiche man:</H2Hoverable>
 	<ul>
 		<li>
-			Günter Kochendörfer/ Bernd Schirok: Maschinelle Textrekonstruktion. Theoretische Grundlegung,
-			praktische Erprobung an einem Ausschnitt des ›Parzival‹ Wolframs von Eschenbach und Diskussion
-			der literaturgeschichtlichen Ergebnisse, Göppingen 1976 (Göppinger Arbeiten zur Germanistik
-			185), S. 112–119
+			Kochendörfer, Günter / Schirok, Bernd: Maschinelle Textrekonstruktion. Theoretische
+			Grundlegung, praktische Erprobung an einem Ausschnitt des ›Parzival‹ Wolframs von Eschenbach
+			und Diskussion der literaturgeschichtlichen Ergebnisse, Göppingen 1976 (Göppinger Arbeiten zur
+			Germanistik 185), S. 112–119.
 		</li>
 		<li>
 			Peter Jörg Becker: Handschriften und Frühdrucke mittelhochdeutscher Epen. Eneide, Tristrant,
 			Tristan, Erec, Iwein, Parzival, Willehalm, Jüngerer Titurel, Nibelungenlied und ihre
-			Reproduktion und Rezeption im späteren Mittelalter und in der frühen Neuzeit, Wiesbaden 1977
+			Reproduktion und Rezeption im späteren Mittelalter und in der frühen Neuzeit. Wiesbaden 1977.
 		</li>
 		<li>
-			Gesa Bonath/ Helmut Lomnitzer: Verzeichnis der Fragment-Überlieferung von Wolframs ›Parzival‹,
-			in: Studien zu Wolfram von Eschenbach. FS Werner Schröder zum 75. Geburtstag, hrsg. v. Kurt
-			Gärtner/ Joachim Heinzle, Tübingen 1989, S. 87–149
+			Bonath, Gesa / Lomnitzer, Helmut: Verzeichnis der Fragment-Überlieferung von Wolframs
+			›Parzival‹. In: Studien zu Wolfram von Eschenbach. FS Werner Schröder zum 75. Geburtstag, hg.
+			von Kurt Gärtner und Joachim Heinzle. Tübingen 1989, S. 87–149
 		</li>
 		<li>
-			Joachim Heinzle: Klassiker-Edition heute, in: Methoden und Probleme der Edition
+			Heinzle, Joachim: Klassiker-Edition heute. In: Methoden und Probleme der Edition
 			mittelalterlicher deutscher Texte. Bamberger Fachtagung 26.–29. Juni 1991. Plenumsreferate,
-			hrsg. v. Rolf Bergmann/ Kurt Gärtner u. a., Tübingen 1993 (Beihefte zu editio 4), S. 50–62
+			hg. von Rolf Bergmann, Kurt Gärtner u. a. Tübingen 1993 (Beihefte zu editio 4), S. 50–62.
 		</li>
 		<li>
-			Bernd Schirok: Parzivalrezeption im Mittelalter, Darmstadt 1982 (Erträge der Forschung 174),
-			S. 30–56
+			Klein, Klaus: Beschreibendes Verzeichnis der Handschriften. In: Wolfram von Eschenbach. Ein
+			Handbuch, hg. von Joachim Heinzle, 2 Bde. Berlin / Boston 2011, Bd. 2, S. 941–1002.
 		</li>
 		<li>
-			Bernd Schirok: Parzival III.1. Die Handschriften und die Entwicklung des Textes, in: Wolfram
-			von Eschenbach. Ein Handbuch, hg. von Joachim Heinzle, 2 Bde., Berlin/ Boston 2011, Bd. 1, S.
-			308–334
+			Bernd Schirok, Bern: Parzivalrezeption im Mittelalter. Darmstadt 1982 (Erträge der Forschung
+			174), S. 30–56.
 		</li>
 		<li>
-			Thomas Franz Schneider und Gabriel Viehhauser: Zwei Neufunde zu Wolframs von Eschenbach
+			Schirok, Bernd: Parzival III.1. Die Handschriften und die Entwicklung des Textes. In: Wolfram
+			von Eschenbach. Ein Handbuch, hg. von Joachim Heinzle, 2 Bde. Berlin/ Boston 2011, Bd. 1, S.
+			308–334.
+		</li>
+		<li>
+			Schneider, Thomas Franz / Viehhauser, Gabriel: Zwei Neufunde zu Wolframs von Eschenbach
 			›Parzival‹. Teil 2: Das dreispaltige Solothurner Fragment F 69. Ein Vertreter der
-			'Nebenfassung' *m, in: Wolfram-Studien 20 (2008), S. 457–525 (mit Abdruck) und Abb. 5–40
+			'Nebenfassung' *m. In: Wolfram-Studien 20 (2008), S. 457–525 (mit Abdruck) und Abb. 5–40.
 		</li>
 		<li>
 			Wolfram von Eschenbach, Parzival. Abbildungen und Transkriptionen zur gesamten
-			handschriftlichen Überlieferung des Prologs, hrsg. v. Uta Ulzen, Göppingen 1974 (Litterae 34)
+			handschriftlichen Überlieferung des Prologs, hg. von Uta Ulzen. Göppingen 1974 (Litterae 34).
 		</li>
+		<li>
+			Wolfram von Eschenbach von Karl Lachmann, 7. Ausgabe. Neu bearbei-tet und mit einem
+			Verzeichnis der Eigennamen und Stammtafeln versehen, Bd. 1: Lieder, Par-zival, Titurel, hg.
+			von Eduard Hartl. Berlin 1952.
+		</li>
+
 		<li>
 			Wolfram von Eschenbach, Parzival. Studienausgabe. Mittelhochdeutscher Text nach der sechsten
 			Ausgabe von Karl Lachmann. Übersetzung von Peter Knecht. Einführung zum Text von Bernd
-			Schirok, Berlin/ New York 1998
+			Schirok. Berlin/ New York 1998.
 		</li>
 		<li>
-			<a href="http://www.handschriftencensus.de/" target="_blank">
-				Handschriftencensus deutschsprachiger Handschriften des 13. und 14. Jahrhunderts
-			</a>
+			Wolfram von Eschenbach: ›Parzival‹, Überlieferung (Handschriftencensus) – <a
+				href="http://www.handschriftencensus.de/werke/437"
+				target="_blank"
+			>
+				Handschriftencensus</a
+			>. Eine Bestandsaufnahme der handschriftlichen Überlieferung deutschsprachiger Texte des
+			Mittelalters
 		</li>
 	</ul>
-	<p>
-		Die Handschriften-Siglen entsprechen den Angaben bei Klaus Klein: Beschreibendes Verzeichnis der
-		Handschriften, in: Wolfram von Eschenbach. Ein Handbuch, hg. von Joachim Heinzle, 2 Bde.,
-		Berlin/ Boston 2011, Bd. 2, S. 941–1002, hier: 943–959 [in Klammern die Siglen der Systematik
-		nach Hartl (Studienausgabe von Schirok, S. XXVIIff.)].
-	</p>
-	<p>
-		Die Fragmentbezeichnung in Ziffern schließt sich G. Bonath/H. Lomnitzer, Verzeichnis der
-		Fragment-Überlieferung, an [in Klammern ältere Siglen (vgl. u. a. Ulzen, S. VIf.)].
-	</p>
 </div>
--- src/routes/mitarbeitende[[rest]]/+page.svelte
+++ src/routes/mitarbeitende[[rest]]/+page.svelte
@@ -3,137 +3,97 @@
 </script>
 
 <div class="container mx-auto typography">
-	<h1>Mitarbeiterinnen und Mitarbeiter am Parzival-Projekt</h1>
+	<h1>Mitarbeitende des Parzival-Projekts</h1>
 
 	<H2Hoverable name="Projektleitung"></H2Hoverable>
 
 	<ul>
 		<li>
-			<a href="https://parzival.unibe.ch/stolz/index1.html" target="_blank">
-				Prof. Dr. Michael Stolz
-			</a> (Universität Bern)
+			<a href="mailto:michael.stolz@unibe.ch"> Prof. Dr. Michael Stolz </a> (Universität Bern)
 		</li>
 	</ul>
 
-	<H2Hoverable name="Wissenschaftliche Mitarbeiterinnen und Mitarbeiter"></H2Hoverable>
+	<H2Hoverable name="Wissenschaftliche Ko-Leitung und Ansprechpartner"></H2Hoverable>
 
 	<ul>
 		<li>
-			<a
-				href="https://www.germanistik.unibe.ch/ueber_uns/personen/pd_dr_abel_stefan/index_ger.html"
-				target="_blank">PD Dr. Stefan Abel</a
-			> (Universität Bern)
+			<a href="mailto:stefan.abel@unibe.ch"> PD Dr. Stefan Abel </a> (Universität Bern)
 		</li>
+	</ul>
+
+	<H2Hoverable name="Ko-Leitung im D-A-CH-Projekt (2012–2017)"></H2Hoverable>
+
+	<ul>
+		<li>Prof. Dr. Jens Haustein (Friedrich-Schiller-Universität Jena)</li>
+		<li>PD Dr. Sonja Glauch (Friedrich-Alexander-Universität Erlangen-Nürnberg)</li>
+		<li>Prof. Dr. Martin Schubert (Berlin-Brandenburgische Akademie der Wissenschaften)</li>
 	</ul>
 
 	<H2Hoverable name="Programmierung und Webdesign"></H2Hoverable>
 	<ul>
 		<li>Simone Hiltscher (Universitäten Basel und Bern, 2005 bis 2021)</li>
+
+		<li>Nicolas Kessler (Universität Bern, unterstützend 2018/19)</li>
+
+		<li>Dominique Steinbach (Universität Bern, 2021 bis 2026)</li>
+
+		<li>Philipp Lammerskitten (Universität Bern, 2021 bis 2026)</li>
+
 		<li>
 			<a
 				href="https://www.dh.unibe.ch/ueber_uns/personen/prof_dr_hodel_tobias/index_ger.html"
 				target="_blank">Prof. Dr. Tobias Hodel</a
-			> (Digital Humanities, Universität Bern, 2024)
+			> (Digital Humanities, Universität Bern, 2024–2026)
 		</li>
+
 		<li>
 			<a
 				href="https://www.dh.unibe.ch/ueber_uns/personen/daengeli_peter/index_ger.html"
 				target="_blank">Peter Dängeli</a
-			> (Digital Humanities, Universität Bern, 2024)
+			> (Digital Humanities, Universität Bern, 2024–2026)
 		</li>
 		<li>
 			<a
 				href="https://www.dh.unibe.ch/ueber_uns/personen/flick_sebastian/index_ger.html"
 				target="_blank">Sebastian Flick</a
-			> (Digital Humanities, Universität Bern, 2024)
+			> (Digital Humanities, Universität Bern, 2024–2026)
 		</li>
-	</ul>
-	<H2Hoverable name="Wartung und Support"></H2Hoverable>
-	<ul>
-		<li>Dominique Steinbach (Universität Bern)</li>
-		<li>Philipp Lammerskitten (Universität Bern)</li>
 	</ul>
 
 	<H2Hoverable name="Ehemalige wissenschaftliche Mitarbeiterinnen und Mitarbeiter"></H2Hoverable>
 
 	<ul>
-		<li>
-			<a href="https://www.glw.uni-jena.de/mitarbeiter-innen/haustein-jens" target="_blank"
-				>Prof. Dr. Jens Haustein</a
-			> (Universität Jena, Ko-Projektleitung DFG 2012–2017)
-		</li>
-		<li>
-			<a
-				href="https://www.germanistik.phil.fau.de/faecher/mediaevistik/lehrstuhl-fuer-aeltere-deutsche-literatur/univisid/49922054/"
-				target="_blank">PD Dr. Sonja Glauch</a
-			> (Universität Erlangen-Nürnberg, Ko-Projektleitung DFG 2012–2017)
-		</li>
 		<li>Letizia Carigiet M.A. (Universität Bern)</li>
 		<li>Dr. Yen-Chun Chen (Universität Bern)</li>
 		<li>
-			<a
-				href="https://www.germanistik.uni-bonn.de/institut/abteilungen/skandinavische-sprachen-und-literaturen/abteilung/personal/chlench-priber-kathrin"
-				target="_blank">Prof. Dr. Kathrin Chlench</a
-			> (Universität Bern, jetzt W3-Professorin an der Universität Bonn)
+			Prof. Dr. Kathrin Chlench (Universität Bern, jetzt Rheinische Friedrich-Wilhelms-Universität
+			Bonn)
 		</li>
 		<li>Dr. Ann-Kathrin Deininger (Universität Bern)</li>
-		<li>
-			<a
-				href="https://www.germanistik.unibe.ch/ueber_uns/personen/dr_strieder_miriam/index_ger.html"
-				target="_blank">Dr. Miriam Strieder</a
-			> (Universität Bern)
-		</li>
+		<li>Jasmin Eggel M.A. (Universität Bern)</li>
 		<li>Dr. Agata Mazurek (Universität Bern)</li>
-		<li>
-			<a href="https://www.ku.de/suche?tx_solr%5Bq%5D=simon+falch#tx-solr-search" target="_blank"
-				>Dr. Simon Falch</a
-			> (Universität Bern)
-		</li>
-		<li>Sonja Falk M.A. (Universität Erlangen-Nürnberg)</li>
-		<li>Dr. Richard Fasching (Universität Bern, jetzt Universität Zürich)</li>
+		<li>Dr. Simon Falch (Universität Bern)</li>
+		<li>Sonja Falk M.A. (Friedrich-Alexander-Universität Erlangen-Nürnberg)</li>
+		<li>Dr. Richard Fasching (Universität Bern)</li>
 		<li>Marius Gehrig M.A. (Universität Bern)</li>
 		<li>Dr. Mirjam Geissbühler</li>
-		<li>
-			Christian Griesinger M.A. (Universität Bern, jetzt Lehrbeauftragter an der Universität
-			Wuppertal)
-		</li>
 		<li>Christiane Klein M.A. (Universität Bern)</li>
-		<li>Nicolas Kessler (Universität Bern)</li>
 		<li>Dr. Jörg Klenk (Berlin-Brandenburgische Akademie der Wissenschaften)</li>
-		<li>Simone Lahn M.A. (Universität Erlangen-Nürnberg)</li>
-		<li>Dr. Judith Lange (Universität Bern, jetzt Universität Duisburg-Essen)</li>
-		<li>Dr. Claudia Lingscheid (Universität Bern, jetzt in der Verwaltung der Universität Oslo)</li>
+		<li>Simone Lahn M.A. (Friedrich-Alexander-Universität Erlangen-Nürnberg)</li>
+		<li>Dr. Judith Lange (Universität Bern)</li>
+		<li>Dr. Claudia Lingscheid (Universität Bern)</li>
 		<li>Dr. Agata Mazurek (Universität Bern)</li>
-		<li>Dr. Christoph Pretzer (Universität Bern, jetzt Marie Curie-Fellow)</li>
+		<li>Dr. Christoph Pretzer (Universität Bern)</li>
 		<li>Anett Rehner M.A. (Berlin-Brandenburgische Akademie der Wissenschaften)</li>
+		<li>Livia Sandri M.A. (Berlin-Brandenburgische Akademie der Wissenschaften)</li>
+		<li>PD Dr. Robert Schöller (Universität Bern)</li>
 		<li>
-			Livia Sandri M.A. (Berlin-Brandenburgische Akademie der Wissenschaften, jetzt
-			Projektmitarbeiterin Burgerbibliothek Bern)
-		</li>
-		<li>
-			<a
-				href="https://www.germanistik.unibe.ch/ueber_uns/personen/pd_dr_schoeller_robert/index_ger.html"
-				target="_blank">PD Dr. Robert Schöller</a
-			> (Universität Bern)
-		</li>
-		<li>
-			<a
-				href="https://www.uni-due.de/zim/services/suchdienste/mitarbeiter.php?id=58239&vcard="
-				target="_blank">Prof. Dr. Martin Schubert</a
-			> (Berlin-Brandenburgische Akademie der Wissenschaften, jetzt Professor für Germanistische Mediävistik,
+			Prof. Dr. Martin Schubert(Berlin-Brandenburgische Akademie der Wissenschaften, jetzt
 			Universität Duisburg-Essen)
 		</li>
 		<li>Christian Sonder M.A. (Universität Bern)</li>
-		<li>Dr. Miriam Strieder (Universität Bern, jetzt Universität Bonn)</li>
-		<li>
-			<a href="http://www.uni-stuttgart.de/dh/Abteilung/Mitarbeiter/Viehhauser.html" target="_blank"
-				>Prof. Dr. Gabriel Viehhauser</a
-			> (Universität Bern, jetzt W3-Professor für Digital Humanities an der Universität Stuttgart)
-		</li>
-		<li>
-			Dr. Lydia Wegener (Universität Bern, jetzt Wissenschaftliche Mitarbeiterin, Deutsche Texte
-			Mittelalter, Berlin-Brandenburgische Akademie der Wissenschaften, Lehrbeauftragte
-			Humboldt-Univerität zu Berlin)
-		</li>
+		<li>Dr. Miriam Strieder (Universität Bern)</li>
+		<li>Prof. Dr. Gabriel Viehhauser (Universität Bern, jetzt Universität Wien)</li>
+		<li>Dr. Lydia Wegener (Universität Bern)</li>
 	</ul>
 </div>
