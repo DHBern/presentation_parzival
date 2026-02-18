2c2ebf5dc18426ee3a4190fa2460280ef1a913a6
flicksolutions <sebastian@flick.solutions>
2026-02-18 11:42:23 +0100

    setting basepath to ""

--- src/routes/einzelverssynopse/data/[handle]/[thirties]/[verse]/+server.js
+++ src/routes/einzelverssynopse/data/[handle]/[thirties]/[verse]/+server.js
@@ -10,24 +10,4 @@ export async function GET({ fetch, params }) {
 	const item = await res.json();
 
 	return json(item);
-
-	// return json({
-	// 	format: 'json',
-	// 	view: 'page',
-	// 	doc: 'd.xml',
-	// 	root: null,
-	// 	rootNode: '3.4.2.211',
-	// 	id: 'd_1.02',
-	// 	odd: 'parzival.odd',
-	// 	next: '3.4.2.423',
-	// 	previous: '3.4.2.198',
-	// 	nextId: 'd006',
-	// 	previousId: 'd005',
-	// 	switchView: '3.4.2',
-	// 	content:
-	// 		'<!DOCTYPE html>\n<div class="content "><div class="tei-l" id="d_1.02">daz muͦz der ſele werden ſur</div></div>',
-	// 	footnotes: '',
-	// 	userParams: { webcomponents: 7 },
-	// 	collection: ''
-	// });
 }
