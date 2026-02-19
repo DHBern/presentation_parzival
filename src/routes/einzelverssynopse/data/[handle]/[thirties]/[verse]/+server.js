6785e004c621eb69ad6c8a49b15cec62666a0054
flicksolutions <sebastian@flick.solutions>
2026-02-19 09:31:47 +0100

    test to see if static response works

--- src/routes/einzelverssynopse/data/[handle]/[thirties]/[verse]/+server.js
+++ src/routes/einzelverssynopse/data/[handle]/[thirties]/[verse]/+server.js
@@ -9,7 +9,26 @@ export async function GET({ fetch, params }) {
 
 	const item = await res.json();
 
-	return json(item, { status: res.status });
+	// return json(item, { status: res.status });
+	return json({
+		format: 'json',
+		view: 'page',
+		doc: 'g.xml',
+		root: null,
+		rootNode: '3.4.2.253',
+		id: 'g_2.11-ii',
+		odd: 'parzival.odd',
+		next: '3.4.2.475',
+		previous: '3.4.2.164',
+		nextId: 'g001v',
+		previousId: 'g001r',
+		switchView: '3.4.2',
+		content:
+			'<!DOCTYPE html>\n<div class="content "><div class="tei-l" id="g_2.11-ii">ſi entwichent vn<span class="tei-choice1">(de)</span> chernt.</div></div>',
+		footnotes: '',
+		userParams: { webcomponents: 7 },
+		collection: ''
+	});
 }
 
 export const prerender = true;
